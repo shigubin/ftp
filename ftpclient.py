bd3625d23463c1250de54c0a48aa28dbbd86ce33
shigubin <35258223+shigubin@users.noreply.github.com>
2018-01-11 10:01:49 +0800

    Add files via upload

--- ftpclient.py
+++ ftpclient.py
@@ -9,22 +9,61 @@ class FtpClient:
     def __init__(self):
         self.sock = socket.socket()
         self.sock.connect((HOST, PORT))
+        self.prompt = ">>>:"
 
     def login(self, cmd_list):
+        self.username = cmd_list[1]
         cmd_dict = {"action":cmd_list[0], "username":cmd_list[1], "password":cmd_list[2]}
         self.sock.sendall(json.dumps(cmd_dict).encode("utf-8"))
 
+        data = self.sock.recv(1024)
+        msg_dict = json.loads(data.decode())
+        if msg_dict["ret"] == True:
+            self.prompt = self.username + '@ftpserver-' + msg_dict["cur_dir"] + ':'
+        else:
+            print("sorry! auth failed")
+
+    def show(self, cmd_list):
+        cmd_dict = {"action": cmd_list[0]}
+        self.sock.sendall(json.dumps(cmd_dict).encode("utf-8"))
+
+        data = self.sock.recv(1024)
+        data = data.decode()
+        print(self.prompt + data)
+
+    def cd(self, cmd_list):
+        cmd_dict = {"action":cmd_list[0], "dir":cmd_list[1]}
+        self.sock.sendall(json.dumps(cmd_dict).encode("utf-8")) 
+
+        data = self.sock.recv(1024)
+        msg_dict = json.loads(data.decode())
+
+        if msg_dict["ret"] == True:
+            self.prompt = self.username + '@ftpserver-' + msg_dict["cur_dir"] + ':'
+        else:
+            print(self.prompt+"change dirtory failed!")
+
+    def makedir(self, cmd_list):
+        cmd_dict = {"action":cmd_list[0], "dir":cmd_list[1]}
+        self.sock.sendall(json.dumps(cmd_dict).encode("utf-8"))
+
+        data = self.sock.recv(1024)
+        msg = data.decode()
+
+        print(self.prompt + msg)
+
     def run(self):
         while True:
-            cmd_line = input(">>>:").strip()
+            cmd_line = input(self.prompt).strip()
+            if not cmd_line:
+                continue
+
             cmd_list = cmd_line.split()
 
             if hasattr(self, cmd_list[0]):
                 func = getattr(self, cmd_list[0])
                 func(cmd_list)
 
-            data = self.sock.recv(1024)
-            print(data.decode())
 
 if __name__ == "__main__":
     ftpClient = FtpClient()
--- ftpserver.py
+++ ftpserver.py
@@ -2,7 +2,7 @@ import os
 import socketserver
 import json
 import configparser
-
+ 
 
 class FtpServer(socketserver.BaseRequestHandler):
 
@@ -19,12 +19,66 @@ class FtpServer(socketserver.BaseRequestHandler):
 
     def login(self, cmd_dict):
         if self.auth(cmd_dict["username"], cmd_dict["password"]):
-            self.request.sendall("auth suc".encode("utf-8"))
+            welcome = "welcome to ftpserver %s" % cmd_dict["username"]
+            self.pwd = os.getcwd()
+            self.pwd += '/'
+            self.homedir = cmd_dict["username"]
+            if not os.path.exists(self.pwd+self.homedir):
+                os.mkdir(self.pwd+self.homedir)
+
+            self.curdir = self.homedir
+            os.chdir(self.pwd+self.curdir)
+
+            msg_dict = {"ret":True, "cur_dir":self.curdir}
+            self.request.sendall(json.dumps(msg_dict).encode("utf-8"))
+
         else:
+            msg_dict = {"ret":False}
             self.request.sendall("auth failed".encode("utf-8"))
 
-    def show(self):
-        pass
+    def show(self, cmd_dict):
+        file_list = os.listdir(self.pwd + self.curdir)
+        file_str = ''
+        for file in file_list:
+            file_str += (file + ' ')
+
+        self.request.sendall(file_str.encode('utf-8'))
+
+    def cd(self, msg_dict):
+        chdir = msg_dict["dir"]
+        suc = False
+
+        if chdir == ".":
+            suc = True
+        elif chdir == "..":
+            if 1 < len(self.curdir.split("\\")):
+                self.curdir = self.curdir.rsplit("\\", 1)[0]
+                os.chdir(self.pwd+self.curdir)
+                suc = True
+            else:
+                print("already in the home dirtory")
+        else:
+            ab_chdir = self.pwd+self.curdir+'\\'+chdir
+            if os.path.exists(ab_chdir):
+                os.chdir(self.pwd+self.curdir+'\\'+chdir)
+                self.curdir = self.curdir+'\\'+chdir
+                suc = True
+            else:
+                pass
+
+        if suc == True:
+            msg_dict = {"ret":True, "cur_dir":self.curdir}
+            self.request.sendall(json.dumps(msg_dict).encode('utf-8'))
+        else:
+            msg_dict = {"ret":False}
+            self.request.sendall(json.dumps(msg_dict).encode('utf-8'))
+
+    def makedir(self, msg_dict):
+        if not os.path.exists(self.curdir + msg_dict["dir"]):
+            os.mkdir(msg_dict["dir"])
+            self.request.sendall("make dir succ".encode('utf-8'))
+        else:
+            self.request.sendall("make dir failed!".encode('utf-8'))
 
     def handle(self):
         while True:
